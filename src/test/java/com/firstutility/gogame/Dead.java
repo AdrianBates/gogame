77f444dafadf2ba1f6d4842a09f7f1890f2ad33a
Adrian Bates <adrian.bates@first-utility.com>
2015-12-23 12:16:18 +0000

    Completed single black square scenarios

--- src/test/java/com/firstutility/gogame/Dead.java
+++ src/test/java/com/firstutility/gogame/Dead.java
@@ -4,13 +4,15 @@ public class Dead {
 
 	public static boolean isDead(int x, int y, int[][] board) {
 		
-		if (board[0][1] == 2 && board[1][0] == 2) {
+		if (blocked(x, y-1, board) && blocked(x, y+1, board) && blocked(x-1, y, board) && blocked(x+1, y, board)) {
 			return true;
 		}
-		if (board[0][0] == 2 && board[2][0] == 2 && board[1][1] == 2) {
-			return true;
-		}
+		
 		return false;
 	}
 
+	private static boolean blocked(int x, int y, int[][] board) {
+		return x < 0 || x == board[0].length || y < 0 || y == board.length || board[y][x] == 2;
+	}
+	
 }
--- src/test/java/com/firstutility/gogame/DeadTest.java
+++ src/test/java/com/firstutility/gogame/DeadTest.java
@@ -7,55 +7,156 @@ import org.junit.Test;
 
 public class DeadTest {
 
-	// Black in Top Left Corner Tests
+	// Black piece at 11
 	@Test
-	public void testBlackPieceInCorner_NoWhite_NotDead() {
-		int[][] board = {{1,0}, 
-				         {0,0}};
-		assertFalse("Should be alive", isDead(0,0, board));
+	public void testBlackPiece11_NoWhite_NotDead() {
+		int[][] board = {{0,0,0}, 
+				         {0,1,0},
+				         {0,0,0}};
+		assertFalse("Should be alive", isDead(1,1, board));
 	}
 
 	@Test
-	public void testBlackPieceInCorner_WhiteToTheRight_NotDead() {
-		int[][] board = {{1,2}, 
-				         {0,0}};
-		assertFalse("Should be alive", isDead(0,0, board));
+	public void testBlackPiece11_WhiteAbove_NotDead() {
+		int[][] board = {{0,2,0}, 
+				         {0,1,0},
+				         {0,0,0}};
+		assertFalse("Should be alive", isDead(1,1, board));
 	}
 
 	@Test
-	public void testBlackPieceInCorner_SurroundedByWhite_Dead() {
-		int[][] board = {{1,2}, 
+	public void testBlackPiece11_WhiteAboveBelow_NotDead() {
+		int[][] board = {{0,2,0}, 
+				         {0,1,0},
+				         {0,2,0}};
+		assertFalse("Should be alive", isDead(1,1, board));
+	}
+
+	@Test
+	public void testBlackPiece11_WhiteAboveBelowLeft_NotDead() {
+		int[][] board = {{0,2,0}, 
+				         {2,1,0},
+				         {0,2,0}};
+		assertFalse("Should be alive", isDead(1,1, board));
+	}
+
+	@Test
+	public void testBlackPiece11_WhiteAboveBelowLeftRight_Dead() {
+		int[][] board = {{0,2,0}, 
+				         {2,1,2},
+				         {0,2,0}};
+		assertTrue("Should be dead", isDead(1,1, board));
+	}
+
+	// Black piece at 21
+	@Test
+	public void testBlackPiece21_NoWhite_NotDead() {
+		int[][] board = {{0,0,0,0}, 
+				         {0,0,1,0},
+				         {0,0,0,0}};
+		assertFalse("Should be alive", isDead(2,1, board));
+	}
+
+	@Test
+	public void testBlackPiece21_WhiteAbove_NotDead() {
+		int[][] board = {{0,0,2,0}, 
+				         {0,0,1,0},
+				         {0,0,0,0}};
+		assertFalse("Should be alive", isDead(2,1, board));
+	}
+
+	@Test
+	public void testBlackPiece21_WhiteAboveBelow_NotDead() {
+		int[][] board = {{0,0,2,0}, 
+				         {0,0,1,0},
+				         {0,0,2,0}};
+		assertFalse("Should be alive", isDead(2,1, board));
+	}
+
+	@Test
+	public void testBlackPiece21_WhiteAboveBelowLeft_NotDead() {
+		int[][] board = {{0,0,2,0}, 
+				         {0,2,1,0},
+				         {0,0,2,0}};
+		assertFalse("Should be alive", isDead(2,1, board));
+	}
+
+	@Test
+	public void testBlackPiece21_WhiteAboveBelowLeftRight_Dead() {
+		int[][] board = {{0,0,2,0}, 
+				         {0,2,1,2},
+				         {0,0,2,0}};
+		assertTrue("Should be dead", isDead(2,1, board));
+	}
+
+	// Prove that the prod code is taking the x and y values into account
+	@Test
+	public void testBlackPiece31_WhiteSurrounding11_Dead() {
+		int[][] board = {{0,2,0,0,0}, 
+				         {2,0,2,1,0},
+				         {0,2,0,0,0}};
+		assertFalse("Should be alive", isDead(3,1, board));
+	}
+	
+	// Prove that the prod code can cope with the piece being on the edge
+	@Test
+	public void testBlackPieceOnTopRow_WhiteSurrounding_Dead() {
+		int[][] board = {{2,1,2},
+				         {0,2,0}};
+		assertTrue("Should be dead", isDead(1,0, board));
+	}
+	
+	@Test
+	public void testBlackPieceOnLeftHandSide_WhiteSurrounding_Dead() {
+		int[][] board = {{2,0},
+				         {1,2},
+				         {2,0}};
+		assertTrue("Should be dead", isDead(0,1, board));
+	}
+	
+	@Test
+	public void testBlackPieceOnRightHandSide_WhiteSurrounding_Dead() {
+		int[][] board = {{0,2},
+				         {2,1},
+				         {0,2}};
+		assertTrue("Should be dead", isDead(1,1, board));
+	}
+	
+	@Test
+	public void testBlackPieceOnBottomRow_WhiteSurrounding_Dead() {
+		int[][] board = {{0,2,0},
+				         {2,1,2}};
+		assertTrue("Should be dead", isDead(1,1, board));
+	}
+
+	// Prove corners work
+	@Test
+	public void testBlackPieceTopLeftCorner_WhiteSurrounding_Dead() {
+		int[][] board = {{1,2},
 				         {2,0}};
 		assertTrue("Should be dead", isDead(0,0, board));
 	}
 
-	// Back on Top Edge, on in from right edge
 	@Test
-	public void testBlackPieceOnTopEdge_NoWhite_NotDead() {
-		int[][] board = {{0, 1, 0}, 
-				         {0, 0, 0}};
-		assertFalse("Should be alive", isDead(1,0, board));
-	}
-
-	@Test
-	public void testBlackPieceOnTopEdge_WhiteToRight_NotDead() {
-		int[][] board = {{0, 1, 2}, 
-				         {0, 0, 0}};
-		assertFalse("Should be alive", isDead(1,0, board));
-	}
-
-	@Test
-	public void testBlackPieceOnTopEdge_WhiteToBothRightAndLeft_NotDead() {
-		int[][] board = {{2, 1, 2}, 
-				         {0, 0, 0}};
-		assertFalse("Should be alive", isDead(1,0, board));
-	}
-
-	@Test
-	public void testBlackPieceOnTopEdge_SurroundedByWhite_Dead() {
-		int[][] board = {{2, 1, 2}, 
-				         {0, 0, 0}};
+	public void testBlackPieceTopRightCorner_WhiteSurrounding_Dead() {
+		int[][] board = {{2,1},
+				         {0,2}};
 		assertTrue("Should be dead", isDead(1,0, board));
 	}
 
+	@Test
+	public void testBlackPieceBottomRightCorner_WhiteSurrounding_Dead() {
+		int[][] board = {{0,2},
+				         {2,1}};
+		assertTrue("Should be dead", isDead(1,1, board));
+	}
+
+	@Test
+	public void testBlackPieceBottomLeftCorner_WhiteSurrounding_Dead() {
+		int[][] board = {{2,0},
+				         {1,2}};
+		assertTrue("Should be dead", isDead(0,1, board));
+	}
+
+	
 }
