da9c4d7137d81de3cb1b9a6fc787a1fab5da3f38
Adrian Bates <adrian.bates@first-utility.com>
2015-12-23 10:14:57 +0000

    First Commit

---
+++ src/main/java/com/firstutility/gogame/App.java
@@ -0,0 +1,13 @@
+package com.firstutility.gogame;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
